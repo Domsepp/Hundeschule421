7023c6bed0d8bca67b2ad782460f4d30bcc7f8b4
Domsepp <paulpanterodt4@gmail.com>
2022-04-11 19:15:35 +0200

    Adjustments for Mobile Layout

--- mainscript.js
+++ mainscript.js
@@ -9,6 +9,8 @@
 
 var popup ='';
 
+var visible = 0;
+
 function showPopup1(popupname) {
         popup = popupname;
         document.getElementById('overlay').classList.add('active');
@@ -17,4 +19,15 @@ function showPopup1(popupname) {
 function removePopup1() {
         document.getElementById('overlay').classList.remove('active');
         document.getElementById(popup).classList.remove('active');
+}
+
+function dropMenu(){
+  if(visible == 0){
+    document.getElementById('DropdownMenu').classList.add('active');
+    visible = 1;
+  }
+  else{
+    document.getElementById('DropdownMenu').classList.remove('active');
+    visible = 0;
+  }
 }
