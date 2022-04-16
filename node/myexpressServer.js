28c3f5da2772add5377142578fddb5b78fb307fc
Domsepp <paulpanterodt4@gmail.com>
2022-04-16 18:13:09 +0200

    fist express experiments

--- node/myexpressServer.js
+++ node/myexpressServer.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.all("*", (req,res) => {
-    console.log(req);
-    res.send("hello world!!!");
+app.get("/", (req,res) => {                             // Wenn nichts hinten dran steht, soll es einfach ein get request machen
+    res.send("hello GET world!!!");
+});
+app.get("/users/", (req,res) => {                             // Wenn nichts hinten dran steht, soll es einfach ein get request machen
+    res.send("hello user world!!!");
 });
 
 app.listen(1337);
