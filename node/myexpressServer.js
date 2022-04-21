450dc389d3bcb978cf70c7b249d29277d979a06b
Domsepp <paulpanterodt4@gmail.com>
2022-04-21 17:45:12 +0200

    express folders continued

--- node/myexpressServer.js
+++ node/myexpressServer.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
 app.get("/", (req,res) => {                             // Wenn nichts hinten dran steht, soll es einfach ein get request machen
     res.send("hello GET world!!!");
 });
-app.get("/users/", (req,res) => {                             // Wenn nichts hinten dran steht, soll es einfach ein get request machen
+app.get("/users/", (req,res) => {              
     res.send("hello user world!!!");
 });
 
 app.listen(1337);
-console.log("Sever listening on port 1337")
+console.log("Sever listening on port 1337");
