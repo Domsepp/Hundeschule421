184f3ccdf5f22143cd53528c21af59404768cf18
Domsepp <paulpanterodt4@gmail.com>
2022-04-24 12:05:52 +0200

    first login param implemented

--- node/myexpressServer.js
+++ node/myexpressServer.js
@@ -6,13 +6,25 @@ app.use(express.static('pages/styles'));
 app.use(express.static('pages/Images'));
 app.use(express.static('pages/scripts'));
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 function mainPage(req,res,next){
     res.sendFile(path.join(__dirname + "/pages/index.html"));
 }
 
+function getLogin(req,res,next){
+    console.log(req.body['name']);
+    console.log(req.body['password']);
+    res.send("<h1>Hallo Post</h1>");
+}
 
-app.use("/", mainPage);
+
+app.post("/api/users/",getLogin);
+app.get("/", mainPage);
+
 
 app.listen(1337);
 console.log("Sever listening on port 1337");
