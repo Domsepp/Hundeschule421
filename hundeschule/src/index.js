42469f0bec77a351b1f1c85bb6f6dff3c864b3e1
Domsepp <paulpanterodt4@gmail.com>
2022-08-07 17:59:58 +0200

    timer created

--- hundeschule/src/index.js
+++ hundeschule/src/index.js
@@ -1,21 +1,21 @@
-// eslint-disable-next-line
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// eslint-disable-next-line
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//let counter = 0;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  /*<React.StrictMode>
-    <App />
-  </React.StrictMode>*/
-  <h1>Hello World</h1>
+const counter = ReactDOM.createRoot(document.getElementById('counter'));
 
-);
+root.render(<h1>Hello World</h1>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let counterVal = 0;
+
+function displaytime(){
+  counterVal++;
+  counter.render(<h1>{counterVal}</h1>);
+}
+
+setInterval(displaytime,1000);
+
+
