62d2ff857ecc3eeffb2fda1c60d2dec65ef2ee79
Domsepp <paulpanterodt4@gmail.com>
2022-08-07 16:40:01 +0200

    minor changes

--- hundeschule/src/index.js
+++ hundeschule/src/index.js
@@ -1,14 +1,18 @@
+// eslint-disable-next-line
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// eslint-disable-next-line
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  /*<React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>*/
+  <h1>Hello World</h1>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
