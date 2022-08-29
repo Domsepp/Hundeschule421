788cdb9e69238dbb559da318a223e7a9d299e3d9
Domsepp <paulpanterodt4@gmail.com>
2022-08-29 16:31:41 +0200

    more practice

--- hundeschule/src/index.js
+++ hundeschule/src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import { useState } from 'react';
+//import {useEffect } from 'react';
 
 //let counter = 0;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// const counter = ReactDOM.createRoot(document.getElementById('counter'));
+const fun = ReactDOM.createRoot(document.getElementById('functionalComp'));
+const moin = ReactDOM.createRoot(document.getElementById('moin'));
+const buttonDiv = ReactDOM.createRoot(document.getElementById('buttonDiv'));
+const listDiv = ReactDOM.createRoot(document.getElementById('nameList'));
 
 class Moin extends React.Component{
+
   render(){
     return <h3>Moin mit Klasse</h3>;
   }
 }
 
+function popup(){
+    moin.render(<div>
+      <h1>Hello There</h1>
+    </div>);
+}
 
 class NameNum extends React.Component{
   state = {
@@ -38,10 +50,6 @@ class NameNum extends React.Component{
   }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// const counter = ReactDOM.createRoot(document.getElementById('counter'));
-const fun = ReactDOM.createRoot(document.getElementById('functionalComp'));
-const moin = ReactDOM.createRoot(document.getElementById('moin'));
 
 root.render(<h1>Hello World</h1>);
 
@@ -60,9 +68,43 @@ function Displaytime(){
   }
 }
 
+function NameList(props){
+  const nameArr = props.data;
+  const ListItems = nameArr.map((val, index)=><li key={index}>{val}</li>);
+  return <ul>{ListItems}</ul>
+}
+
+function Greet(){
+  const[name, setName] = useState("Default");
+  function changeIsHere(e){
+    setName(e.target.value);
+  }
+  function chooseGender(){
+    if(["Neubert","Rausch"].includes(name)) return "Sehr geehrte Frau "+name;
+    else return "Sehr geehrter Herr "+name;
+  }
+  function addNameTolist(e){
+    names.push(name);
+    e.preventDefault();
+    listDiv.render(<NameList data={names}/>);
+  }
+
+  return <div>
+  <form onSubmit={addNameTolist}>
+  <input type="text" value={name} onChange={changeIsHere}></input>
+  <input type='submit' value="Submit"/>
+  </form>
+  <p>{chooseGender(name)}</p>
+  </div>;
+}
+
+const greeter = <Greet/>;
+root.render(greeter)
 
 const moinel = <Moin />;
 moin.render(moinel);
+buttonDiv.render(<button onClick={popup}>back</button>)
+
 setInterval(Displaytime,1000);
 
 
